9201843d7a414f449086d93c2dd4296e8359c47d
Devansh Soni <devanshsoni108@gmail.com>
2026-08-19 22:56:33 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = s.strip().lower().replace(" ", "")
+
+        # processed_string = "".join(char for char in string if char.isalnum())
+        processed_string = ""
+        for item in string:
+            if item.isalnum():
+                processed_string = processed_string+item
+
+        reversed_string = processed_string[::-1]
+        for index in range(0, len(processed_string)):
+            if processed_string[index] != reversed_string[index]:
+                return False
+
+        return True
